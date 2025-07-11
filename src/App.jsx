229d7b80f37a11372cac0e30c3825a396497d608
shivam2144 <shivamdalve123@gmail.com>
2025-07-12 02:40:31 +0530

    url chage

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ const appRouter = createBrowserRouter([
     path: "/profile",
     element: <Profile />
   },
+  
   // admin ke liye yha se start hoga
   {
     path:"/admin/companies",
--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,6 +1,7 @@
-const  url="https://job-portal-backend-ikfb.onrender.com";
+ const  url="https://job-portal-backend-ikfb.onrender.com";
+// const url = "http://localhost:8000";
 
 export const USER_API_END_POINT=`${url}/api/v1/user`;
 export const JOB_API_END_POINT=`${url}/api/v1/job`;
 export const APPLICATION_API_END_POINT=`${url}/api/v1/application`;
-export const COMPANY_API_END_POINT=`{url}/api/v1/company`;
+export const COMPANY_API_END_POINT=`${url}/api/v1/company`;
